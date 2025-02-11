ed43039b0cd59a5fbf5ec1eff16524cf5358991b
JM_Clase <1922635@alu.murciaeduca.es>
2025-02-11 19:19:23 +0100

    más cosas creo

--- app/src/main/java/com/example/proyectofinal/LoginFragment.kt
+++ app/src/main/java/com/example/proyectofinal/LoginFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider.NewInstanceFactory.Companion.instance
+import com.example.proyectofinal.databinding.FragmentLoginBinding
+import com.google.android.material.snackbar.Snackbar
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -29,13 +32,21 @@ class LoginFragment : Fragment() {
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+    private lateinit var binding: FragmentLoginBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?
+    {
+        binding = FragmentLoginBinding.inflate(layoutInflater)
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?)
+    {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+
 
     companion object {
         /**
@@ -55,5 +66,19 @@ class LoginFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+
+        private var instance: LoginFragment? = null
+
+        fun getInstance(): Fragment {
+            if (instance == null)
+            {
+                instance = LoginFragment()
+            }
+
+
+            return instance!!
+        }
+
+        class FragmentUno private constructor(): Fragment()
     }
 }
