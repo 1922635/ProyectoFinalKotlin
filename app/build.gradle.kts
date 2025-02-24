2930898e6f8cf52aa4d02d6845aa94ec3d184ffa
JM_Clase <1922635@alu.murciaeduca.es>
2025-02-24 19:09:25 +0100

    Ahora funciona la app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,10 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    //Navigation
+    implementation ("androidx.navigation:navigation-fragment-ktx:2.5.3")
+    implementation ("androidx.navigation:navigation-ui-ktx:2.5.3")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/proyectofinal/MainActivity.kt
+++ app/src/main/java/com/example/proyectofinal/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.proyectofinal
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
 import com.example.proyectofinal.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity()
@@ -18,16 +20,31 @@ class MainActivity : AppCompatActivity()
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        
 
-        cargarFragment(LoginFragment.getInstance())
-        var numFragment = 1
+    }
+
+    //Nuevo metodo para configurar el componente Navigation
+    override fun onSupportNavigateUp(): Boolean
+    {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
+                    as NavHostFragment
+
+
+        val navController = navHostFragment.navController
+
+
+        return navController.navigateUp() ||
+                super.onSupportNavigateUp()
+
 
     }
 
     private fun cargarFragment(fragment: Fragment)
     {
         val fragmentTransaction = supportFragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.frameLayout, fragment)
+        fragmentTransaction.replace(R.id.nav_host_fragment, fragment)
         fragmentTransaction.commit()
     }
 }
